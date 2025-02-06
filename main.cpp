e34de87bc4671afae08ded471dafdceb38c71a52
wba6 <bradenaey1@gmail.com>
2025-02-06 16:53:09 -0500

    Add large integer library

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
+#include "gmpxx.h"
 #include <iostream>
 
 int main() {
+    mpz_t my_large_integer;
+    mpz_init(my_large_integer);
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
