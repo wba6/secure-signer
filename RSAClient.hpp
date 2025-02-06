729c75f269d5b124f15f808b7ef547aee962786c
wba6 <bradenaey1@gmail.com>
2025-02-06 17:16:21 -0500

    Add layout

--- RSAClient.hpp
+++ RSAClient.hpp
@@ -6,9 +6,25 @@
 
 #ifndef RSACLIENT_HPP
 #define RSACLIENT_HPP
+#include <gmp.h>
+
 
 class RSAClient {
+public:
+    RSAClient();
+    ~RSAClient();
 
+    void encrypt(std::string& message, mpz_t& returnVal);
+    void decrypt(mpz_t& message, std::string& returnVal);
+private:
+    void generatePrime(mpz_t& returnVal);
+    void generatePrivateKey(mpz_t& returnVal);
+    void generatePublicKey(mpz_t& returnVal);
+
+    void saveKeysToFile(std::string& filename);
+private:
+    mpz_t p;
+    mpz_t q;
 };
 
 #endif // RSACLIENT_HPP
